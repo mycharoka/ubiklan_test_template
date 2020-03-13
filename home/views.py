affd098dddd7c6b0c1223eea79b3c633da57f16f
Muhammad Vidi <muhammadvidi.m@gmail.com>
2020-03-13 13:19:20 +0700

    first commit

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'banner':'We innovate ways that put technology to out-of-home ads.',
+        'sub_banner':'It\'s a modern twist to the traditional.',
+    }
+    return render(request, 'index.html', context)
