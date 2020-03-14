ed726794c7753d809b9219f6ea0a176708a17c38
Muhammad Vidi <muhammadvidi.m@gmail.com>
2020-03-14 20:39:29 +0700

    commit

--- home/views.py
+++ home/views.py
@@ -5,5 +5,8 @@ def index(request):
     context = {
         'banner':'We innovate ways that put technology to out-of-home ads.',
         'sub_banner':'It\'s a modern twist to the traditional.',
+        'footer':'Mall Taman Palem, Parkiran Lantai 8',
+        'address_top' : 'Jl. Kamal Raya Cengkareng Timur, Kec. Cengkareng',
+        'address_bottom': ' Jakarta Barat DKI Jakarta, 11730',
     }
     return render(request, 'index.html', context)
